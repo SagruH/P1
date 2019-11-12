e3edd435305e8714a50c9b294d688349974616bc
SagruH <sagru2899@googlemail.com>
2019-11-12 12:07:02 +0100

    py erstellt

---
+++ em/2emp1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import PhyPraKit as ppk #wurde von mir verändert
+import matplotlib.pyplot as plt
+
+import kafe
+from kafe.function_tools import FitFunction, LaTeX, ASCII
+from kafe.function_library import quadratic_3par
+
+import uncertainties as uc
