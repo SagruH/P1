aa2c63e1fb2e11c5f2c1a21ffa2f24540e1c46f2
SagruH <sagru2899@googlemail.com>
2019-11-12 12:39:31 +0100

    read CSV x/y

--- em/2emp1.py
+++ em/2emp1.py
@@ -8,5 +8,23 @@ from kafe.function_library import quadratic_3par
 
 import uncertainties as uc
 
-def abc:
-    return;
+def loadCSV(x):
+    hlines, data = ppk.readCSV(x,2)
+    data = np.array(data)
+    U,I=np.split(data,2)
+    U = U[0]
+    I = I[0]
+    return U,I;
+
+def xPlatte():
+    Ux,Ix = loadCSV("2_2x.csv")
+    print(Ux,Ix)
+    return
+
+def yPlatte():
+    Uy,Iy = loadCSV("2_2y.csv")
+    print(Uy,Iy)
+    return
+
+yPlatte()
+xPlatte()
