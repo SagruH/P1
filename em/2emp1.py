672c50fe9dd59658431c054f989f7a208b8f71d3
SagruH <sagru2899@googlemail.com>
2019-11-13 16:20:01 +0100

    add linreg

--- em/2emp1.py
+++ em/2emp1.py
@@ -2,6 +2,8 @@ import numpy as np
 import PhyPraKit as ppk #wurde von mir verändert
 import matplotlib.pyplot as plt
 
+from scipy import stats
+
 import kafe
 from kafe.function_tools import FitFunction, LaTeX, ASCII
 from kafe.function_library import quadratic_3par
@@ -31,6 +33,12 @@ def BFeld(I):
 
 def edmcalc(U,B,d):
     edm = (8*np.pi()*U)/(B**2*d**2)
+    return;
+
+def linregfunc(m,b):
+    x=np.linspace(0,0.19,1000)
+    f = m*x+b
+    return f,x;
 
 def xyPlatte():
     Ux,Ix = loadCSV("2_2x.csv")
@@ -39,19 +47,29 @@ def xyPlatte():
     Ix2 = Ix**2
     Iy2 = Iy**2
 
-    dx
-    dy
+    xslope, intercept, xr_value, p_value, xstd_err = stats.linregress(Ix2,Ux)
+    fx, xx = linregfunc(xslope,intercept)
 
-    plt.plot(Ux,Ix2, label = "x-Platten")
-    plt.plot(Uy,Iy2, label = "y-Platten")
-    plt.xlabel("U in V")
-    plt.ylabel("I² in A²")
+    yslope, intercept, yr_value, p_value, ystd_err = stats.linregress(Iy2,Uy)
+    fy, xy = linregfunc(yslope,intercept)
+
+    print("r auf x: " , xr_value , "r auf y: " , yr_value)
+    print("m von x: " , xslope , "m von y: " , yslope)
+
+    plt.plot(xx,fx, label = "x-linreg")
+    plt.plot(Ix2,Ux ,"og" , label = "x-Platten",)
+
+    plt.plot(xy,fy, label = "y-linreg")
+    plt.plot(Iy2,Uy, "or" , label = "y-Platten")
+
+    plt.ylabel("U in V")
+    plt.xlabel("I² in A²")
     plt.title("Aufgabe 2 e/m-Bestimmung")
     plt.grid(True)
     plt.legend()
-    #plt.show()
+    plt.show()
     return
 
 
 
-#xyPlatte()
+xyPlatte()
