0645773da9a9c23a8509f980da1c41dce057c1aa
SagruH <sagru2899@googlemail.com>
2019-12-12 16:28:23 +0100

    lib add num int calc

--- general/library.py
+++ general/library.py
@@ -26,3 +26,20 @@ def minmaxfind(a):
     HP=np.int_(HP)
     TP=np.int_(TP)
     return HP,TP;
+
+
+def num_int_calc(d, S): #needs test
+    a = np.linspace(d,S,10000)
+    da = a[100]-a[99]
+    lamda = np.array([])
+    Integral= 0
+    for i in a:
+        y = lamdafunc(i) #name der funktion unter der integriert wird
+        lamda = np.hstack((lamda,y))
+
+    for j in np.arange(len(lamda)):
+        if j>0:
+            Integral = Integral + (lamda[j-1]*da) + (lamda[j]-lamda[j-1])*da*1/2
+
+    lamdaq = (1/(S-d))*Integral
+    return lamdaq;
